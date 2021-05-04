731121374f04b2d8d254a2a090b2bf0459aeb83a
andrew blotski <andrew.blotski@globant.com>
2021-05-04 11:29:47 +0300

    add logiin module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }];
+const routes: Routes = [{ path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }, { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
